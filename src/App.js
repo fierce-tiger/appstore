56349008af7dfa555c01802b55dc9cc474b188dc
tiger <ziyan.yuan@stonybrook.edu>
2023-01-26 17:49:09 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+// import Login part of Login file
+import React, {useState} from "react";
+import Login from "./component/Login/Login";
+import Appstore from "./component/Appstore/Appstore";
 
 function App() {
+  const [login, setLogin] = useState(false);
+
+    const HandleLogin = () => {
+        setLogin(true)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h2>Appstore</h2>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+          {!login ? <Login handleLogin={HandleLogin}/> : <Appstore/>}
+      </div>
   );
 }
 
---
+++ src/component/Login/Antd.js
@@ -0,0 +1,84 @@
+import {Button, Checkbox, Form, Input} from 'antd';
+
+// const onFinish = (values) => {
+//     console.log('Success:', values);
+// };
+const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+const App = ({onFinish, onRegister}) => {
+
+    return (
+        <div>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8,
+                }}
+                wrapperCol={{
+                    span: 16,
+                }}
+                style={{
+                    maxWidth: 600,
+                }}
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item
+                    label="Username"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username!',
+                        },
+                    ]}
+                >
+                    <Input/>
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password!',
+                        },
+                    ]}
+                >
+                    <Input.Password/>
+                </Form.Item>
+
+                <Form.Item
+                    name="remember"
+                    valuePropName="checked"
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item
+                    wrapperCol={{
+                        offset: 8,
+                        span: 16,
+                    }}
+                >
+                    <Button block={true} type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                    <Button block={true} type="dashed" onClick={onRegister}>register</Button>
+                </Form.Item>
+            </Form>
+        </div>
+
+    )
+};
+export default App;
